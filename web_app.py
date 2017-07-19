884093b986514e64522533fa5cb55f51f9212ea4
Badeaa <marjieh.badea@gmail.com>
2017-07-19 12:22:06 +0200

    flask app with gmaps

--- web_app.py
+++ web_app.py
@@ -2,7 +2,6 @@ from flask import Flask, url_for, flash, render_template, redirect, request, g, 
 from flask import session as login_session
 from werkzeug.utils import secure_filename
 import locale, os
-import googlemaps
 from datetime import datetime
 
 UPLOAD_FOLDER = 'uploads'
@@ -24,7 +23,7 @@ def allowed_file(filename):
 
 @app.route("/")
 def Index():
-	return render_template("Index.html")
+	return render_template("rpoint.html")
 
 @app.route("/login")
 def SignIn():
